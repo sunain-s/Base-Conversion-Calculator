d7a59f0a3745c4cf48f1d01a14ff8ed5050c5f91
Asianguy123 <sas@drsyed.org>
2021-10-07 11:37:14 +0100

    Update conversion_calc.py

--- conversion_calc.py
+++ conversion_calc.py
@@ -1,3 +1,10 @@
+def leading_zeros(binary, length):
+    binary = binary[::-1]
+    if len(binary) % length:
+        zeros = length - (len(binary) % length)
+        binary += '0' * zeros
+    return binary
+
 def bin_to_den(binary):
     binary = binary[::-1]
     denary = 0
@@ -7,3 +14,15 @@ def bin_to_den(binary):
             denary += 2**power
         power += 1
     return denary
+
+def den_to_bin(denary, leading):
+    binary = ''
+    if denary < 0:
+        denary *= -1
+    while denary > 0:
+        remainder = denary % 2
+        binary += str(remainder)
+        denary //= 2
+    binary = leading_zeros(binary, leading)
+    binary = binary[::-1]
+    return binary
